8dad90e06ca37995a30454f652e8d188fb6e1c6b
tudorAng <tudoranis@yahoo.com>
2023-08-25 12:58:46 +0300

    add genezio challange instructions to implement delete btn

--- client/src/app.js
+++ client/src/app.js
@@ -5,40 +5,40 @@ import { TaskService } from "./sdk/taskService.sdk.js";
 // If the user has no token yet, we generate a random one.
 let token = localStorage.getItem("apiToken");
 if (!token) {
-    // generate a random token
-    token =
-        Math.random().toString(36).substring(2, 15) +
-        Math.random().toString(36).substring(2, 15);
-    localStorage.setItem("apiToken", token);
+  // generate a random token
+  token =
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15);
+  localStorage.setItem("apiToken", token);
 }
 
 // Function that adds a new task
 async function handleAdd() {
-    document.getElementById("modal-error-elem").innerHTML = "";
+  document.getElementById("modal-error-elem").innerHTML = "";
 
-    // take taskTitle from the input field with id="task-title-input"
-    const taskTitle = document.getElementById("task-title-input").value;
+  // take taskTitle from the input field with id="task-title-input"
+  const taskTitle = document.getElementById("task-title-input").value;
 
-    if (!taskTitle) {
-        // show an error message
-        document.getElementById("modal-error-elem").innerHTML =
-            "Title is mandatory";
-        return;
+  if (!taskTitle) {
+    // show an error message
+    document.getElementById("modal-error-elem").innerHTML =
+      "Title is mandatory";
+    return;
+  }
+
+  // create a new task with the title and the token from local storage using the SDK
+  TaskService.createTask(token, taskTitle).then((res) => {
+    if (res.success) {
+      // reload the page
+      location.reload();
     }
-
-    // create a new task with the title and the token from local storage using the SDK
-    TaskService.createTask(token, taskTitle).then((res) => {
-      if (res.success) {
-        // reload the page
-        location.reload();
-      }
-    });
+  });
 }
 
 // add an event listener to the button with id="add-task-btn"
 document.getElementById("add-task-btn").addEventListener("click", async (e) => {
-    e.preventDefault();
-    handleAdd();
+  e.preventDefault();
+  handleAdd();
 });
 
 // iterate over all tasks
@@ -57,16 +57,22 @@ TaskService.getAllTasksByUser(token).then((res) => {
       taskContainer.innerHTML += `
         <div class="mb-3">
           <div class="d-flex align-items-center">
-              <input type="checkbox" ${
-                task.solved ? 'checked' : ''
-              } class="task_checkbox" id=${task.id}>
+              <input type="checkbox" ${task.solved ? 'checked' : ''
+        } class="task_checkbox" id=${task.id}>
               <p class="mb-0" style="margin-right: auto; margin-left: 20px">
                 ${taskTitle}
                 ${taskLink}
               </p>
-          </div>
-        </div>
-       `;
+            <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="Check out genezio challenge">
+              <button class="btn" onclick="TaskService.deleteTask('${token}', '${task.id}')">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#DC3545" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+              </svg>
+              </button>
+            </span>
+              </div>
+              </div>
+              `;
     }
   }
 
